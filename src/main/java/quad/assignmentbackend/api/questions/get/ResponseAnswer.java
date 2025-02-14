e0795f2d21e3ba7de43539b2a2a451779c402c06
Dennis Rosenbaum <dennis.rosenbaum@outlook.com>
2025-02-14 22:01:17 +0100

    Update to more logical property names

--- src/main/java/quad/assignmentbackend/api/questions/get/ResponseAnswer.java
+++ src/main/java/quad/assignmentbackend/api/questions/get/ResponseAnswer.java
@@ -1,6 +1,6 @@
 package quad.assignmentbackend.api.questions.get;
 
 public record ResponseAnswer(
-        String answerId,
-        String answer) {
+        String id,
+        String title) {
 }
